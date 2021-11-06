806107c5e7710a61edc64f7bf7016f7350e30ec1
siobhanlgorman <siobhanlgorman@gmail.com>
2021-11-06 10:01:58 +0000

    Complete home page header and css

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ## Bugs and Solutions
+1. base.css not found - file was not in css folder - just under it!
+2. background image not loading - 
 
--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -62,7 +62,7 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
             os.path.join(BASE_DIR, 'templates'),
-            os.path.join(BASE_DIR, 'templates', 'allauth')
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -152,8 +152,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = (os.path.join(BASE_DIR, 'media'),)
