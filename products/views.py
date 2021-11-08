a0f75edfdfd2d751e28be43da0cd866ffbf5d260
siobhanlgorman <siobhanlgorman@gmail.com>
2021-11-08 11:21:18 +0000

    Add sorting

--- products/views.py
+++ products/views.py
@@ -12,8 +12,23 @@ def all_products(request):
     products = Product.objects.all()
     query = None
     categories = None  # check if it exists
+    sort = None
+    direction = None
 
     if request.GET:
+
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
 
         """ if category exists split into list at commas and filter current queryset
         of all products down to list of only products whose category name in
@@ -35,10 +50,13 @@ def all_products(request):
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'products': products,
         'search_term': query,
         'current categories': categories,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'products/products.html', context)
