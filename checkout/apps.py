088a94bdccfe1e09d6219e9a8a43d86db144d704
siobhanlgorman <siobhanlgorman@gmail.com>
2021-11-10 11:33:13 +0000

    Add forms, admin and signals

--- checkout/apps.py
+++ checkout/apps.py
@@ -2,5 +2,8 @@ from django.apps import AppConfig
 
 
 class CheckoutConfig(AppConfig):
+    name = 'checkout
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'checkout'
+
+    def ready(self):
+        import checkout.signals
