7bcda933422b77e541d098db244fecab76229a1d
siobhanlgorman <siobhanlgorman@gmail.com>
2021-11-10 14:27:31 +0000

    Add checkout views and template

--- checkout/apps.py
+++ checkout/apps.py
@@ -2,8 +2,7 @@ from django.apps import AppConfig
 
 
 class CheckoutConfig(AppConfig):
-    name = 'checkout
-    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'checkout'
 
     def ready(self):
         import checkout.signals
--- checkout/models.py
+++ checkout/models.py
@@ -1,5 +1,3 @@
-from django.db import models
-
 import uuid
 
 from django.db import models
