0b15b3fd7d235d4767db7c70f8ca41eb68519254
siobhanlgorman <siobhanlgorman@gmail.com>
2021-11-30 16:43:16 +0000

    Update postgres db

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = 'Development' in os.environ
+DEBUG = 'Development' in os.environ
 DEBUG = True
 
 ALLOWED_HOSTS = ['slg-boutique-ado.herokuapp.com', 'localhost']
@@ -135,14 +135,15 @@ if 'DATABASE_URL' in os.environ:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-else:
+    
+# else:
 
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'db.sqlite3',
+#         }
+#     }
 
 
 # Password validation
